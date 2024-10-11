5ae6e2911ce330106b56b69b3b8e8789dcf8f6d2
Nicolas Toconas <ntoconas@lexim.solutions>
2024-10-11 18:30:55 -0300

    Agregado el componente modal y el seteo de la recipe seleccionada

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,11 +1,12 @@
 import { StateCreator } from "zustand"
 import { getCategories, getRecipeById, getRecipes } from "../services/RecipeService"
-import type { Categories, Drinks, SearchFilter, Drink } from "../types"
+import type { Categories, Drinks, SearchFilter, Drink, Recipe } from "../types"
 
 
 export type RecipesSliceType = {
-    categories: Categories,
+    categories: Categories
     drinks: Drinks
+    selectedRecipe: Recipe
     fetchCategories: () => Promise<void>,
     searchRecipes: (SearchFilter : SearchFilter) => Promise<void>
     selectRecipe: (id: Drink["idDrink"]) => Promise<void>
@@ -18,6 +19,7 @@ export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
     drinks: {
         drinks: []
     },
+    selectedRecipe : {} as Recipe,
     fetchCategories: async () => {
         const categories = await getCategories()
         set({
@@ -32,6 +34,8 @@ export const createRecipesSlice : StateCreator<RecipesSliceType> = (set) => ({
     },
     selectRecipe: async (id) => {
         const selectedRecipe = await getRecipeById(id)
-        console.log(selectedRecipe)
+        set({
+            selectedRecipe
+        })
     }
 })
