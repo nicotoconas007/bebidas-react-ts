97104bb573a66e17592f9a2dc37694fb43eb0eec
Nicolas Toconas <ntoconas@lexim.solutions>
2024-10-11 15:13:49 -0300

    Creado el state para tomar la categoria y el ingrediente en el header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
-import { useEffect, useMemo } from "react"
+import { ChangeEvent, useEffect, useMemo, useState } from "react"
 import { NavLink, useLocation } from "react-router-dom"
 import { useAppStore } from "../stores/useAppStore"
 
 export default function Header() {
 
+  const [searchFilters, setSearchFilters] = useState({
+    ingredient: "",
+    category: ""
+  })
   const { pathname } = useLocation()
   const isHome = useMemo(() => pathname === "/" ,[pathname])
 
@@ -12,7 +16,14 @@ export default function Header() {
 
   useEffect(() => {
     fetchCategories()
-  },[])
+  }, [])
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    setSearchFilters({
+      ...searchFilters,
+      [e.target.name] : e.target.value
+    })
+  }
 
   return (
     <header className={isHome ? "bg-header bg-center bg-cover" : "bg-slate-800" }>
@@ -52,19 +63,23 @@ export default function Header() {
                     name="ingredient"
                     className="p-3 w-full rounded-lg focus:outline-none"
                     placeholder="Nombre o Ingrediente. Ej. Vodka, Tequila"
+                    onChange={handleChange}
+                    value={searchFilters.ingredient}
                   />
                 </div>
                 <div className="space-y-4">
                   <label 
-                    htmlFor="categories"
+                    htmlFor="category"
                     className="block text-white uppercase font-extrabold text-lg"
                   >Categoría
                   </label>
 
                   <select
-                    id="categories"
-                    name="categories"
+                    id="category"
+                    name="category"
                     className="p-3 w-full rounded-lg focus:outline-none"
+                    onChange={handleChange}
+                    value={searchFilters.category}
                   >
                     <option value=""> -- Seleccione --</option>
                     {categories.drinks.map( category => (
--- src/views/IndexPage.tsx
+++ src/views/IndexPage.tsx
@@ -1,4 +1,4 @@
-import { useAppStore } from "../stores/useAppStore"
+
 
 export default function IndexPage() {
 
