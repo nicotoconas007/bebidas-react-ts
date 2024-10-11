05f98a321ad252520d241237f2762e3de0a67160
Nicolas Toconas <ntoconas@lexim.solutions>
2024-10-11 20:42:19 -0300

    Agregada la funcionalidad de agregar o eliminar favoritos en el slice store de favorites

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -8,6 +8,8 @@ export default function Modal() {
     const modal = useAppStore((state) => state.modal)
     const closeModal = useAppStore((state) => state.closeModal)
     const selectedRecipe = useAppStore((state) => state.selectedRecipe)
+    const handleClickFavorite = useAppStore((state) => state.handleClickFavorite)
+    const favoriteExists = useAppStore((state) => state.favoriteExists)
     
     const renderIngredientes = () => {
         const ingredients : JSX.Element[] = []
@@ -83,7 +85,11 @@ export default function Modal() {
                     <button
                         type='button'
                         className='w-full rounded bg-orange-600 p-3 font-bold uppercase text-white shadow hover:bg-orange-500'
-                    >Agregar a favoritos</button>
+                        onClick={() => {
+                            handleClickFavorite(selectedRecipe)
+                            closeModal()
+                        }}
+                    >{favoriteExists(selectedRecipe.idDrink) ? "Eliminar de Favoritos" : "Agregar a favoritos"}</button>
                   </div>
                 </DialogPanel>
               </TransitionChild>
