487abeeae834648c2dee6484c6e9445c8eeb8348
Nicolas Toconas <ntoconas@lexim.solutions>
2024-10-11 20:49:19 -0300

    Agregado el loadfromstorage para traer a los favoritos desde el local storage si es que existen

--- src/stores/favoritesSlice.ts
+++ src/stores/favoritesSlice.ts
@@ -5,6 +5,7 @@ export type FavoritesSliceType = {
     favorites: Recipe[]
     handleClickFavorite: (recipe: Recipe) => void
     favoriteExists: (id: Recipe["idDrink"]) => boolean
+    loadFromStorage: () => void
 }
 
 export const createFavoritesSlice : StateCreator<FavoritesSliceType> = (set, get) => ({
@@ -19,8 +20,18 @@ export const createFavoritesSlice : StateCreator<FavoritesSliceType> = (set, get
                 favorites: [...state.favorites, recipe]
             }))
         }
+
+        localStorage.setItem("favorites", JSON.stringify(get().favorites))
     },
     favoriteExists: (id) => {
         return get().favorites.some(favorite => favorite.idDrink === id)
+    },
+    loadFromStorage: () => {
+        const storedFavorites = localStorage.getItem("favorites")
+        if(storedFavorites) {
+            set({
+                favorites: JSON.parse(storedFavorites)
+            })
+        }
     }
 })
