71a01fdc7129c93e855a4be9fd4171526d2146d7
Nicolas Toconas <ntoconas@lexim.solutions>
2024-10-11 17:20:31 -0300

    Agregado el componente para mostrar cada Drink

---
+++ src/components/DrinkCard.tsx
@@ -0,0 +1,27 @@
+import { Drink } from "../types"
+
+type DrinkCardProps = {
+    drink: Drink
+}
+
+export default function DrinkCard({drink} : DrinkCardProps) {
+  return (
+    <div className="shadow-lg rounded-xl bg-slate-200">
+        <div className="overflow-hidden rounded-xl">
+            <img 
+                src={drink.strDrinkThumb} 
+                alt={drink.strDrink} 
+                className="hover:scale-125 transition-transform hover:rotate-2"
+            />
+        </div>
+        <div className="p-5 ">
+            <h2 className="text-2xl truncate font-black">{drink.strDrink}</h2>
+            <button
+                type="button"
+                className="bg-orange-400 hover:bg-orange-500 mt-5 w-full p-3 font-bold text-white text-lg rounded-lg"
+            > Ver Receta
+            </button>
+        </div> 
+    </div>
+  )
+}
