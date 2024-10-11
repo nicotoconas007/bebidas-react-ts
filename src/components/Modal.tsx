c4d6707c087c1d7e3121dadaa4c6f45b21390e5e
Nicolas Toconas <ntoconas@lexim.solutions>
2024-10-11 18:53:48 -0300

    Agregado el nombre imagen e instrucciones de la receta en el modal, iterando en los ingredientes.

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,11 +1,33 @@
 import { Dialog, DialogPanel, DialogTitle, Transition, TransitionChild } from '@headlessui/react';
 import { Fragment } from 'react';
 import { useAppStore } from '../stores/useAppStore';
+import { Recipe } from '../types';
 
 export default function Modal() {
 
     const modal = useAppStore((state) => state.modal)
     const closeModal = useAppStore((state) => state.closeModal)
+    const selectedRecipe = useAppStore((state) => state.selectedRecipe)
+    
+    const renderIngredientes = () => {
+        const ingredients : JSX.Element[] = []
+        for(let i = 1; i <= 6; i++) {
+            const ingredient = selectedRecipe[`strIngredient${i}` as keyof Recipe]
+            const measure = selectedRecipe[`strMeasure${i}` as keyof Recipe]
+
+            if(ingredient && measure) {
+                ingredients.push(
+                    <li
+                        key={i}
+                        className='text-lg font-normal'    
+                    >
+                        {ingredient} - {measure}
+                    </li>
+                )
+            }
+        }
+        return ingredients
+    }
 
   return (
     <>
@@ -34,16 +56,23 @@ export default function Modal() {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl sm:p-6" >
+                <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-xl sm:p-6" >
                   <DialogTitle as="h3" className="text-gray-900 text-4xl font-extrabold my-5 text-center">
-                      Titulo Aquí
+                      {selectedRecipe.strDrink}
                   </DialogTitle>
+                    <img 
+                        src={selectedRecipe.strDrinkThumb} 
+                        alt={selectedRecipe.strDrink}
+                        className='mx-auto w-96 rounded-xl'
+                    />
                   <DialogTitle as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
                     Ingredientes y Cantidades
                   </DialogTitle>
+                  {renderIngredientes()}
                   <DialogTitle as="h3" className="text-gray-900 text-2xl font-extrabold my-5">
                     Instrucciones
                   </DialogTitle>
+                  <p className='text-lg'>{selectedRecipe.strInstructions}</p>
                 </DialogPanel>
               </TransitionChild>
             </div>
