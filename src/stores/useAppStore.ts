dc96906155050c474623c7796050cffa31e5eb21
Nicolas Toconas <ntoconas@lexim.solutions>
2024-10-11 19:21:25 -0300

    Agregado un slice nuevo para el store que almacene los favoritos

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,7 +1,9 @@
 import { create } from "zustand"
 import { createRecipesSlice, RecipesSliceType } from "./recipeSlice"
 import { devtools } from "zustand/middleware"
+import { FavoritesSliceType, createFavoritesSlice } from "./favoritesSlice"
 
-export const useAppStore = create<RecipesSliceType>()(devtools((...a) => ({
-    ...createRecipesSlice(...a)
+export const useAppStore = create<RecipesSliceType & FavoritesSliceType>()(devtools((...a) => ({
+    ...createRecipesSlice(...a),
+    ...createFavoritesSlice(...a)
 })))
